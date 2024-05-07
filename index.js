43f8181ddfcd7c5325bf6f0337e8ec6fbe10bec1
Michael P <102651473+michmich16@users.noreply.github.com>
2024-05-07 10:21:54 +0200

    push

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import http from 'http';
 
+const port = 3000;
+
 http.createServer((request, response) => {
-    console.log('Hello world');
-}).listen(4000)
+    console.log(`Webserver is running now on http://localhost:${port}`);
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	response.write('Hello World');
+	response.end();
+}).listen(port)
