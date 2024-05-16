e3274ad3fcc5d98e3248afb4899070bf504cee95
Michael P <102651473+michmich16@users.noreply.github.com>
2024-05-16 13:58:19 +0200

    push model codes

---
+++ Models/artist.model.js
@@ -0,0 +1,37 @@
+import { supabase } from '../Config/supabase.config.js';
+
+export default class ArtistModel {
+    static async getAllRecords() {
+        try {
+            const { data, error } = await supabase
+                .from('Artist')
+                .select('name, id')
+
+            if(error){
+                throw new Error(error)
+            } else{
+                return data
+            }
+        }catch(error){
+            console.error(`Fejl i kald af artists: ${error}`);
+        }
+    }
+
+    static async getRecordByID(){
+        try{
+            const {data, error } = await supabase
+            .from ('Artist')
+            .select('*')
+            .eq('id',id)
+
+        if(error){
+            throw new Error(error)
+        }else{
+            return data
+        }
+        }catch(error){
+            console.error(`Fejl i kald af artist-liste: ${error}`);
+        }
+    }
+}
+
