7ab25518fcd16484ac304a9b71ccdafa903d1251
Michael P <102651473+michmich16@users.noreply.github.com>
2024-05-23 10:36:27 +0200

    edited index.js

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(SongController)
 
-SongController.get('/songs/:id([0-9A-Za-z]*)', async (req, res) => {
-    console.log(req.params.id)
- })
-
 app.listen(port,()=>{
     console.log(`webserver is running now 0n http://localhost:${port}`);
 })
