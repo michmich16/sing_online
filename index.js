f5158ad34882fcfad38b2cc0aad615882c43bea6
Michael P <102651473+michmich16@users.noreply.github.com>
2024-05-24 12:45:33 +0200

    updated import all controllers to index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { ProfilesController } from './Controllers/profiler.controller.js';
 import { SongController } from './Controllers/song.controller.js';
+import { ArtistController } from './Controllers/artist.controller.js';
+import { AlbumController } from './Controllers/album.controller.js';
 const app = express()
 const port = process.env.PORT
 dotenv.config();
@@ -10,7 +13,10 @@ app.get('/', (req, res) => {
 })
 app.use(express.urlencoded({ extended: true }));
 
+app.use(ProfilesController)
 app.use(SongController)
+app.use(ArtistController)
+app.use(AlbumController)
 
 app.listen(port,()=>{
     console.log(`webserver is running now 0n http://localhost:${port}`);
